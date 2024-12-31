ed0202aa13f714a216e220b1395d0065fa9a4b34
cendyz <arek57823@gmail.com>
2024-12-31 18:47:24 +0100

    StepTwo data

--- src/components/Main/Form/Steps/StepTwo/StepTwo.jsx
+++ src/components/Main/Form/Steps/StepTwo/StepTwo.jsx
@@ -1,7 +1,6 @@
 import styles from './StepTwo.module.scss'
-import arcadeIcon from '../../../../../images/icon-arcade.svg'
-import advancedIcon from '../../../../../images/icon-advanced.svg'
-import proIcon from '../../../../../images/icon-pro.svg'
+import { plansData } from '../../../../../data'
+import { nanoid } from 'nanoid'
 
 const StepTwo = () => {
 	return (
@@ -11,38 +10,18 @@ const StepTwo = () => {
 				You have the option of monthly or yearly billing.
 			</p>
 			<div className={styles.btnsBox}>
-				<button type='button' className={styles.btn}>
-					<img
-						src={arcadeIcon}
-						alt='Joystick icon'
-						className={styles.icon}
-					/>
-					<div className={styles.descBox}>
-						<h3 className={styles.descTitle}>Arcade</h3>
-						<p className={styles.price}>$90/yr</p>
-						<p className={styles.free}>2 months free</p>
-					</div>
-				</button>
-				<button type='button' className={styles.btn}>
-					<img
-						src={advancedIcon}
-						alt='Old console gamepad icon'
-						className={styles.icon}
-					/>
-					<div className={styles.descBox}>
-						<h3 className={styles.descTitle}>Arcade</h3>
-						<p className={styles.price}>$90/yr</p>
-						<p className={styles.free}>2 months free</p>
-					</div>
-				</button>
-				<button type='button' className={styles.btn}>
-					<img src={proIcon} alt='Gamepad icon' className={styles.icon} />
-					<span className={styles.descBox}>
-						<h3 className={styles.descTitle}>Pro</h3>
-						<p className={styles.price}>$150/yr</p>
-						<p className={styles.free}>2 months free</p>
-					</span>
-				</button>
+				{plansData.map(({ title, price, icon, alt }) => {
+					return (
+						<button type='button' className={styles.btn} key={nanoid()}>
+							<img src={icon} alt={alt} className={styles.icon} />
+							<div className={styles.descBox}>
+								<h3 className={styles.descTitle}>{title}</h3>
+								<p className={styles.price}>${price}/yr</p>
+								<p className={styles.free}>2 months free</p>
+							</div>
+						</button>
+					)
+				})}
 			</div>
 		</>
 	)
---
+++ src/data.jsx
@@ -0,0 +1,24 @@
+import arcadeIcon from './images/icon-arcade.svg'
+import advancedIcon from './images/icon-advanced.svg'
+import proIcon from './images/icon-pro.svg'
+
+export const plansData = [
+	{
+		title: 'Arcade',
+		price: 90,
+		icon: arcadeIcon,
+		alt: 'Joystick icon',
+	},
+	{
+		title: 'Advanced',
+		price: 120,
+		icon: advancedIcon,
+		alt: 'Old console gamepad icon',
+	},
+	{
+		title: 'Pro',
+		price: 150,
+		icon: proIcon,
+		alt: 'Gamepad icon',
+	},
+]
